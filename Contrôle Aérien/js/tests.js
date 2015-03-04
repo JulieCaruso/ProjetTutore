4332b13260699836b88e4452bec8aca091b2d597
phuthanhnguyen <phuthanh.nguyen215@gmail.com>
2015-03-04 19:43:05 +0100

    Update tests.js

--- Contrôle Aérien/js/tests.js
+++ Contrôle Aérien/js/tests.js
@@ -1,8 +1,9 @@
 canvasWidth = "579";
 canvasHeight = "436";
+distLimite = 20;
 
 //quand 2 avions se rapprochent
-function testAirProX(avion1, avion2, dist_limite){
+function testAirProX(avion1, avion2){
 	aX1 = avion1.getX();
 	aY1 = avion1.getY();
 	aZ1 = avion1.getZ();
@@ -12,7 +13,7 @@ function testAirProX(avion1, avion2, dist_limite){
 
 	dist_a1_a2 = Math.sqrt(((aX1-aX2)^2)+((aY1-aY2)^2)+((aZ1-aZ2)^2))
 	if (dist_a1_a2 <= dist_limite){
-			avion1.setColor("red");
+		avion1.setColor("red");
 	} else if (avion1.getColor()=="red") {avion2.setColor("blue");}
 }
 
@@ -21,7 +22,8 @@ function testAirProX(avion1, avion2, dist_limite){
 function testAirProXLim(avion){
 	aX1 = avion1.getX();
 	aY1 = avion1.getY();
-	aZ1 = avion1.getZ();
 	
-	if (aX1 <= 0|Ax1 >= parseInt(579)||
+	if (Math.abs(aX1-parseInt(canvasWidth))<=distLimite|Math.abs(aY1-parseInt(canvasHeight))<=distLimite||Math.abs(aX1)<=distLimite||Math.abs(aY1)<=distLimite){
+		avion1.setColor("red");	
+	} else if (avion1.getColor()=="red") {avion2.setColor("blue");}
 }
